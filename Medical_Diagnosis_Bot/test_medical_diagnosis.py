686e3f55486486adbe3589fe7fbaf40f9a8a4964
Alex Best <bestalex132@yahoo.com>
2023-11-25 23:09:24 -0500

    done for the night

--- Medical_Diagnosis_Bot/test_medical_diagnosis.py
+++ Medical_Diagnosis_Bot/test_medical_diagnosis.py
@@ -5,20 +5,46 @@ sever_dehydration = "Sever Dehydration"
 some_dehydration = "Some Dehydration"
 no_dehydration = "No Dehydration"
 empty_string = ""
+patients_and_diagnosis = [
+  "Mike - Severe Dehydration",
+  "Ben - No Dehydration",
+  "Kevin - Some Dehydration"
+]
 
 class TestMedicalDiagnosis(unittest.TestCase) :
 
+  ## Tests for the assess_skin function ##
+
   def test_assess_skin_1(self) :
-    global some_dehydration
     result = medical_diagnosis.assess_skin("1")
     self.assertEqual(result, some_dehydration)
 
   def test_assess_skin_2(self) :
-    global some_dehydration
     result = medical_diagnosis.assess_skin("2")
     self.assertEqual(result, sever_dehydration)
 
   def test_assess_skin(self) :
-    global some_dehydration
     result = medical_diagnosis.assess_skin("")
     self.assertEqual(result, empty_string)
+
+  ## Tests for the assess_eyes function ##
+
+  def test_assess_eyes_1(self) :
+    result = medical_diagnosis.assess_eyes("1")
+    self.assertEqual(result, no_dehydration)
+
+  def test_assess_eyes_2(self) :
+    result = medical_diagnosis.assess_eyes("2")
+    self.assertEqual(result, sever_dehydration)
+
+  def test_assess_eyes(self) :
+    result = medical_diagnosis.assess_eyes("")
+    self.assertEqual(result, empty_string)
+
+  ## Test for the save_new_diagnosis ##
+
+  def test_save_new_diagnosis(self) :
+    new_patient = "Alex - Sever Dehydration"
+    old_length = len(patients_and_diagnosis)
+    patients_and_diagnosis.append(new_patient)
+    self.assertEqual(old_length + 1, len(patients_and_diagnosis))
