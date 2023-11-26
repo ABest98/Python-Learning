34c157be849e042eafecb0d90b655a36530066fb
Alex Best <bestalex132@yahoo.com>
2023-11-25 21:33:43 -0500

    Updating Medical Diagnosis

--- Medical Diagnosis Bot/MedicalDiagnosis.py
+++ Medical Diagnosis Bot/MedicalDiagnosis.py
@@ -9,41 +9,51 @@
 ## Store new diagnoses to the list
 #######################################################################
 
+# Prompts For the questions and selections
 welcome_prompt = "Welcome doctor, what would you like to do today ?\n - To list all patients, press 1\n - To run a new diagnosis, press 2\n - To quit, press q\n"
 
-name_prompt = "What is the patients name ?\n"
+name_prompt = "What is the patient's name ?\n"
 
-appearance_prompt = "How is the patients general appearance ?\n - 1: Normal\n - 2: Irritable or Lethargic\n"
+appearance_prompt = "How is the patient's general appearance ?\n - 1: Normal\n - 2: Irritable or Lethargic\n"
 
-eye_prompt = "How are the patients eyes ?\n - 1: Eyes are Normal or Slightly Sunken\n - 2: Eyes are very sunken\n"
+eye_prompt = "How are the patient's eyes ?\n - 1: Eyes are Normal or Slightly Sunken\n - 2: Eyes are very sunken\n"
+
+skin_prompt = "How is the patient's skin when you pinch it ?\n - 1: Normal Skin Pinch\n - 2: Slow Skin Pinch\n"
 
 sever_dehydration = "Sever Dehydration"
 some_dehydration = "Some Dehydration"
 no_dehydration = "No Dehydration"
 
+# Worker functions
 def list_patients() :
   print("Listing patients and diagnoses")
 
-def access_skin() :
-  print("accessing skin")
+def access_skin(skin) :
+  if skin == "1" :
+    return some_dehydration
+  elif skin == "2" :
+    return sever_dehydration
 
 def access_eyes(eyes) :
   if eyes == "1" :
     return no_dehydration
   elif eyes == "2" :
     return sever_dehydration
-
-def diagnosis_appearance() :
+  
+# Main functions
+def access_appearance() :
   appearance = input(appearance_prompt)
   if appearance == "1" :
     eyes = input(eye_prompt)
-    diagnosis =  access_eyes(eyes)
+    return access_eyes(eyes)
   elif appearance == "2" :
-    access_skin()
+    skin = input(skin_prompt)
+    return access_skin(skin)
 
 def start_new_diagnosis() :
   name = input(name_prompt)
-  diagnosis_appearance()
+  diagnosis = access_appearance()
+  print(name, diagnosis)
 
 def start() :
   # Will rerun prompts till q is pressed then it'll end
